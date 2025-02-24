15b19dfaa3220fd5e19c4c9408326d64870a17e2
Leonard Zitzmann <leonard.zitzmann@s-und-d.de>
2025-02-24 10:37:49 +0100

    Updated

--- fall_details.py
+++ fall_details.py
@@ -15,11 +15,7 @@ SCRIPT_SOURCES = [
     "CW.AufgabenDetails",
     "CW.AnlagenDetails",
     "CW.GespraechDetails",
-    "CW.BegleitungDetails",
-    "PDF_CW.BegleitungList",
-    "PDF_CW.GespraechList",
-    "PDF_CW.AufgabenList",
-    "PDF_CW.AnlagenList"
+    "CW.BegleitungDetails"
 ]
 
 
